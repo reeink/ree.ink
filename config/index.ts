46736256ffa288ecef4708a65ca6c2fe7a4bf77b
reeink <hi@ree.ink>
2023-03-11 00:32:23 +0800

    feat: add homepage and config

--- config/index.ts
+++ config/index.ts
@@ -7,6 +7,22 @@ export const defineConfig = (
     ...conf.base,
     ...(<types.Base>{
       author: "reeink",
+      lang: "en",
+      title: "Reeink's Blog",
+      description: "A Simple Blog Powered by Astro",
+    }),
+  };
+
+  conf.fonts = {
+    ...conf.fonts,
+    ...(<types.Fonts>{
+      google: {
+        fonts: [
+          { name: "Libre Baskerville", type: "serif" },
+          { name: "Noto Serif SC", type: "serif" },
+        ],
+        link: "https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Noto+Serif+SC:wght@400;700&display=swap",
+      },
     }),
   };
 
