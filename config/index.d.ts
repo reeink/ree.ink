8e73e5785f0dd71c007a0cd5c62efd4a3a472936
reeink <hi@ree.ink>
2023-03-11 00:32:23 +0800

    feat: add homepage and config

--- config/index.d.ts
+++ config/index.d.ts
@@ -1,5 +1,30 @@
 export interface Base {
   author?: string /* 作者，默认为`reeink` */;
+
+  /* HTML lang attribute，默认为`en`，中文可用`zh-CN`。 */
+  /* https://en.wikipedia.org/wiki/IETF_language_tag */
+  /* https://html.spec.whatwg.org/multipage/dom.html#the-lang-and-xml:lang-attributes */
+  lang?: string;
+
+  title?: string /* 网站标题 */;
+  
+  description?: string /* 网站描述 */;
+}
+
+export interface Fonts {
+  google?: {
+    fonts: {
+      name: string; /* 字体名称 */
+      type:
+        | "serif"
+        | "sans-serif"
+        | "monospace"
+        | "cursive"
+        | "fantasy"
+        | "system-ui";  /* 字体类型 */
+    }[];
+    link: string; /* 全部字体链接，从 https://fonts.google.com/ 获取*/
+  };
 }
 
 export interface AuthorBadge {
@@ -11,6 +36,9 @@ export interface AuthorBadge {
 export interface Config {
   // 基础配置
   base: Base;
+
+  // 字体配置
+  fonts: Fonts;
 
   // 作者徽章
   authorBadge: AuthorBadge;
---
+++ src/pages/index.md
@@ -0,0 +1,5 @@
+---
+layout: "@/layouts/HomepageLayout.astro"
+---
+
+Hi, here is my blog powered by [Astro](https://astro.build) and other awesome opensource frameworks. Hope you enjoy it!
