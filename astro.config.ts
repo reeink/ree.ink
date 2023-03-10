5e3a4f33d3d499e8e9450a019cc44b5e7c445ad7
reeink <hi@ree.ink>
2023-03-11 00:50:35 +0800

    feat: support mdx and prerender homepage
    mdx integration type error: https://github.com/withastro/astro/issues/6475

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   output: "server",
-  adapter: cloudflare({ mode: "directory" })
+  adapter: cloudflare({
+    mode: "directory",
+  }),
 });
