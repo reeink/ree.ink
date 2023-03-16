1a8de45d2f0c3d03aec1b6c0c30e01d8ebd95d3c
reeink <hi@ree.ink>
2023-03-17 01:00:16 +0800

    feat: test support for pwa

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,27 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    AstroPWA({
+      workbox: { navigateFallback: "/404" },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   output: "server",
   adapter: cloudflare({
     mode: "directory",
   }),
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
+      theme: "css-variables",
       wrap: true,
     },
-  }
+  },
 });
