e38032b595fa584851cb04a7a0f9ffb8c5925261
reeink <hi@ree.ink>
2023-03-16 11:43:58 +0800

    feat: support collections

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from "astro:content";
+
+const postCollection = defineCollection({
+  schema: z.object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    tags: z.array(z.string().url()).optional(),
+    createdTime: z.date().optional(),
+    lastModifiedTime: z.date().optional(),
+    draft: z.boolean().optional().default(false),
+  }),
+});
+
+export const collections = {
+  "posts": postCollection,
+};
