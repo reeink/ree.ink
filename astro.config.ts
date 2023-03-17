b23084ee336a790fdd47cab043c225c176512cd1
reeink <hi@ree.ink>
2023-03-17 22:53:51 +0800

    fix: config pwa

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,4 @@
+import config from "./site.config";
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
@@ -8,9 +9,51 @@ export default defineConfig({
   integrations: [
     mdx(),
     AstroPWA({
-      workbox: { navigateFallback: "/404" },
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: [],
+      manifest: {
+        name: config.base.title,
+        short_name: config.base.title,
+        description: config.base.description,
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "apple-touch-icon-152x152-precomposed.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "safari-pinned-tab.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+          {
+            src: "mstile-150x150.png",
+            sizes: "150x150",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
       devOptions: {
         enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
       },
     }),
   ],
