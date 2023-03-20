aac5bc22863c6cd7029fd38f2de4f1d84da652bb
ReeInk <hi@ree.ink>
2023-03-20 12:53:21 +0800

    fix: use zod coerce to define posts schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,18 @@ import { z, defineCollection } from "astro:content";
 
 const postCollection = defineCollection({
   schema: z.object({
-    title: z.string().optional(),
-    description: z.string().optional(),
-    tags: z.array(z.string().url()).default([]),
-    createdTime: z.string().datetime().default(new Date(0).toISOString()).transform((v) => new Date(v)),
-    modifiedTime: z.string().datetime().default(new Date(0).toISOString()).transform((v) => new Date(v)),
-    publishedTime: z.string().datetime().default(new Date(0).toISOString()).transform((v) => new Date(v)),
-    draft: z.boolean().default(false),
+    title: z.coerce.string().optional(),
+    description: z.coerce.string().optional(),
+    tags: z.array(z.coerce.string().url()).default([]),
+    createdTime: z.coerce.date().default(new Date(0)),
+    modifiedTime: z.coerce.date().default(new Date(0)),
+    publishedTime: z.coerce.date().default(new Date(0)),
+    draft: z.coerce.boolean().default(false),
   }),
 });
 
 export const collections = {
-  "posts": postCollection,
+  posts: postCollection,
 };
 
 export type Collection = keyof typeof collections;
