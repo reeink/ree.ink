e26e250d93f3dc123968116b73ce8e0912d304dd
ReeInk <hi@ree.ink>
2023-07-27 09:48:02 +0800

    chore: add vue deps

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import AstroPWA from "@vite-pwa/astro";
 import UnoCSS from "@unocss/astro";
+import vue from '@astrojs/vue';
 import * as child from "child_process";
 
 const commitHash = child.execSync("git rev-parse HEAD").toString().trim();
@@ -90,6 +91,7 @@ export default defineConfig({
       },
     }),
     UnoCSS(),
+    vue(),
   ],
   markdown: {
     shikiConfig: {
