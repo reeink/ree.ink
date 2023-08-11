3419b9634bcc390511950430e53120911d373505
ree <hi@ree.ink>
2023-08-11 20:52:34 +0800

    fix: comments minify

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -7,20 +7,22 @@ const isDark = useDark();
 </script>
 
 <template>
-    <Giscus v-if="config.comments.type === 'giscus'" 
-        id="giscus-comments"
-        :host="config.comments.host"
-        :repo="config.comments.repo"
-        :repoId="config.comments.repoId"
-        :category="config.comments.category"
-        :categoryId="config.comments.categoryId"
-        :mapping="config.comments.mapping"
-        :term="config.comments.term"
-        :theme="isDark ? config.comments.darkTheme : config.comments.lightTheme"
-        :strict="config.comments.strict"
-        :reactionsEnabled="config.comments.reactionsEnabled"
-        :emitMetadata="config.comments.emitMetadata"
-        :inputPosition="config.comments.inputPosition"
-        :lang="config.comments.lang"
-        :loading="config.comments.loading" />
+    <div class="comments-root">
+        <Giscus v-if="config.comments.type === 'giscus'" 
+            id="giscus-comments"
+            :host="config.comments.host"
+            :repo="config.comments.repo"
+            :repoId="config.comments.repoId"
+            :category="config.comments.category"
+            :categoryId="config.comments.categoryId"
+            :mapping="config.comments.mapping"
+            :term="config.comments.term"
+            :theme="isDark ? config.comments.darkTheme : config.comments.lightTheme"
+            :strict="config.comments.strict"
+            :reactionsEnabled="config.comments.reactionsEnabled"
+            :emitMetadata="config.comments.emitMetadata"
+            :inputPosition="config.comments.inputPosition"
+            :lang="config.comments.lang"
+            :loading="config.comments.loading" />
+    </div>
 </template>
