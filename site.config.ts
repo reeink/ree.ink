619c169d1ef26fa0fd91a76c707008f7818a864a
ree <hi@ree.ink>
2023-09-24 16:07:52 +0800

    config: remove comments

--- site.config.ts
+++ site.config.ts
@@ -7,20 +7,7 @@ export default defineConfig({
   },
 
   comments: {
-    type: "giscus",
-    repo: "reeink/ree.ink",
-    repoId: "R_kgDOJG_UwQ",
-    category: "Comments",
-    categoryId: "DIC_kwDOJG_Uwc4CU8_P",
-    mapping: "pathname",
-    strict: "0",
-    reactionsEnabled: "1",
-    emitMetadata: "0",
-    inputPosition: "top",
-    lang: "en",
-    lightTheme: "light",
-    darkTheme: "dark",
-    loading: "eager",
+    type: "none",
   },
 
   social: {
